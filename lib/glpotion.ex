861f2fd3b1cbf4e841391885bb3f073cd193e446
Equiel-1703 <henriquegabriel22.hg@gmail.com>
2025-05-20 10:50:08 -0300

    Mix setup

---
+++ lib/glpotion.ex
@@ -0,0 +1,18 @@
+defmodule Glpotion do
+  @moduledoc """
+  Documentation for `Glpotion`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Glpotion.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/glpotion_test.exs
@@ -0,0 +1,8 @@
+defmodule GlpotionTest do
+  use ExUnit.Case
+  doctest Glpotion
+
+  test "greets the world" do
+    assert Glpotion.hello() == :world
+  end
+end
